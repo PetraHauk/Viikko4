12b468d8b4c03dc6f1a6cfdfaac99e65d85b5ca4
Petra Haukkala <petra.haukkala@gmail.com>
2024-04-13 21:47:03 +0300

    Assingment 2 Alku

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import api from './api/index.js';
 const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
 app.use('/public', express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/v1', api);
 
 app.get('/', (req, res) => {
     res.send('Welcome to my REST API!');
@@ -13,7 +19,7 @@ app.get('/api/v1/cats', (req, res) => {
     const cat = {
         cat_id: 1,
         name: 'Viski',
-        birthdate: '2001-02-01',
+        birthdate: '2023-02-01',
         weight: 12,
         owner: 3,
         image: 'https://loremflickr.com/320/240/cat',
